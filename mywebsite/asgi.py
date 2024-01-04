ae564d30b837b9ab2242915c3fafb78cdb2cc4ab
ashwabu <abdulazizredi90@gmail.com>
2024-01-04 06:09:20 +0300

    asgi

--- mywebsite/asgi.py
+++ mywebsite/asgi.py
@@ -4,7 +4,7 @@ ASGI config for mywebsite project.
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
@@ -12,15 +12,15 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing  
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mywebsite.settings')
 
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(
+    'http':get_asgi_application(),
+    'websocket':AuthMiddlewareStack(
         URLRouter(
-           chat.routing.websocket_urlpatterns
+            chat.routing.websocket_urlpatterns
         )
     )
 })
