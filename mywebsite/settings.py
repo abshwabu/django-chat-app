681c5b2460aa46db7b1c6f90f687a42968670b4f
ashwabu <abdulazizredi90@gmail.com>
2024-01-04 06:16:53 +0300

    channel layers

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -40,11 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'chat'
 ]
-ASGI_APPLICATION = 'mywebsite.asgi.application'
+ASGI_APPLICATION = "mysite.asgi.application"
 CHANNEL_LAYERS = {
-    'default':{
-        'BACKEND':'channels.layers.InMemoryChannelLayer'
-    }
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
 }
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
